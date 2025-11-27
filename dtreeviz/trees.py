beaebfb55990d29aed7e91471b8baf6317caec3b
tudor.lapusan <tudor.lapusan@gmail.com>
2025-11-27 10:59:14 +0200

    Add sklearn notebook with AI chat integration

--- dtreeviz/trees.py
+++ dtreeviz/trees.py
@@ -32,30 +32,6 @@ NUM_BINS = [
     5, 5, 5, 5, 5,
 ]  # support for 40 classes
 
-import datetime
-
-# Get the current date
-current_date = datetime.datetime.now().date()
-
-# Define the date after which the model should be set to "gpt-3.5-turbo"
-target_date = datetime.date(2024, 6, 12)
-
-# Set the model variable based on the current date
-if current_date > target_date:
-    llm_model = "gpt-3.5-turbo"
-else:
-    llm_model = "gpt-3.5-turbo-0301"
-
-
-def get_completion(prompt, model=llm_model):
-    messages = [{"role": "user", "content": prompt}]
-    response = openai.ChatCompletion.create(
-        model=model,
-        messages=messages,
-        temperature=0,
-    )
-    return response.choices[0].message["content"]
-
 
 class DTreeVizAPI:
     """
